9178fee4419b4cd8a48c7d9280c231ab8998ba11
Rogelio Menco <rogeliomenco4@gmail.com>
2024-03-02 13:46:08 -0500

    fix: update browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
 import Detail from './components/Detail/Detail.jsx';
 import Form from './components/Form/Form';
 
+const basePath = process.env.PUBLIC_URL ?? '/';
+
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={basePath}>
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
@@ -16,7 +18,7 @@ function App() {
           <Route path="/form" element={<Form />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 export default App;
